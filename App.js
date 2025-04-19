63787fe40e37be9d4cae0915794ba757c71762df
milton munoz <livemilton@hotmail.com>
2025-04-19 11:18:51 -0500

    s6- borders in firstApp

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 
 import React from 'react';
-import {View, Text}from 'react-native';
+import {SafeAreaView, Text, View}from 'react-native';
 
 const App = () => {
 
   return (
-    <View>
-       <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView>
+      <View 
+        style={{
+          borderWidth: 1, 
+          borderTopWidth: 5,
+          borderBottomWidth: 10,
+          borderLeftWidth: 3,
+          borderRightWidth: 20,
+          borderColor: '#15099d',
+          borderRadius: 10,
+          borderTopLeftRadius: 20,
+          borderBottomLeftRadius: 20,
+          borderTopRightRadius: 30,
+          borderBottomRightRadius: 30,
+
+          }}>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
   )
 
 }
