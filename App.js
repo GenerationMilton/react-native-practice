3d9c492d8be640d010a3b13418b46b81e1c22ee1
milton munoz <livemilton@hotmail.com>
2025-04-19 18:41:03 -0500

    s7- pops and propTypes in firstApp

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {SafeAreaView, Text, View}from 'react-native';
 import MyText from './components/MyText/MyText';
+import Item from './components/item/item';
 
 const App = () => {
 
@@ -18,7 +19,9 @@ const App = () => {
       <View>
         <Text>All rights reserved</Text>
       </View>
-      
+      <Item name={'table'} price={20}></Item>
+      <Item name={'Chair'} price={100}></Item>
+      <Item name={'Desk'} price={200}></Item>
     </SafeAreaView>
   )
 
