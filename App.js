5217c2640f78dac2cec59d5553eb18fe6b21d2f1
milton munoz <livemilton@hotmail.com>
2025-04-20 22:02:30 -0500

    feat s8 useEffect in first app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, View}from 'react-native';
 import MyText from './components/MyText/MyText';
 import Item from './components/item/item';
 
 const App = () => {
 
+  //UseState
   const [originalText, setText]= useState('Hello, World!')
+  //UseEffect
+  const [text, setText2]=useState(0);
+  useEffect(()=>{
+    console.log('The text has changed!')
+  },[text]);
+  useEffect(()=>{
+    console.log('The component has rendered!')
+  },[])
 
   return (
     <SafeAreaView>
@@ -31,6 +40,12 @@ const App = () => {
         <Text
           onPress={()=>setText('Hello World, I learned how to change state!')}>
           {originalText}
+        </Text>
+      </View>
+      <View>
+        <Text
+          onPress={()=>setText2(text+1)}>
+          {text}
         </Text>
       </View>
     </SafeAreaView>
