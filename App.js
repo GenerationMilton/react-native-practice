90e6230078a519056d88f8f776d0fb6985e7b3c5
milton munoz <livemilton@hotmail.com>
2025-04-25 22:53:15 -0500

    feat s10 building basic components :heavy_plus_sign: - Text input component

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 
 import React,{useState} from 'react';
-import {SafeAreaView, Image} from 'react-native';
+import {SafeAreaView, Image, TextInput, Text, TextComponent} from 'react-native';
 
 const App = () => {
 
+  //hook to manage image state
   const [imageSource, setImageSource] = useState({uri:"http://example.com"});
+  //hook to manage input state
+  const[textValue, setTextValue]= useState('');
+
+  const[passwordValue, setpasswordValue]= useState('');
+  //hook to manage input email
+  const[email, setEmail]= useState('');
+
   return<SafeAreaView>
       <Image 
       source={imageSource} 
@@ -18,6 +26,41 @@ const App = () => {
       <Image 
       source={{uri:"https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80"}} 
       style={{width: 100,height:100}}
+    />
+    
+    <TextInput 
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={textValue}
+      onChangeText={(value)=> {
+        //console.log(value);
+        setTextValue(value);
+      }}
+      autoFocus={true}
+      placeholder={'Please enter your name'}
+    />
+     <TextInput 
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={email}
+      onChangeText={(value)=> {
+        //console.log(value);
+        setEmail(value);
+      }}
+      returnKeyType={'done'}//done //go //next
+      keyboardType={'email-address'}
+      autoFocus={true}
+      placeholder={'Please enter your email'}
+    />
+    <TextInput 
+      style={{borderWidth: 1, padding: 10, borderRadius: 4}}
+      value={passwordValue}
+      onChangeText={(value)=> {
+        //console.log(value);
+        setpasswordValue(value);
+      }}
+      keyboardType={'default'} //'numeric' //'phone-pad' //'default'
+      secureTextEntry={true}
+      autoFocus={true}
+      placeholder={'Please enter your password'}
     />
   </SafeAreaView>
 
