8805452fffb97734701b3f54ddb7b987ea0af0b4
milton munoz <livemilton@hotmail.com>
2025-04-25 21:40:54 -0500

    feat s10 building basic components :heavy_plus_sign: - Image component

--- App.js
+++ App.js
@@ -1,97 +1,26 @@
 
-import React, {useState, useEffect, useRef} from 'react';
-import {Button, SafeAreaView, ScrollView, Text, View}from 'react-native';
-import MyText from './components/MyText/MyText';
-import Item from './components/item/item';
-import { ThemeContext } from './contexts/ThemeContext';
-import HomeScreen from './components/HomeScreeen/HomeScreen';
-import useToggle from './customHooks/useToggle';
+import React,{useState} from 'react';
+import {SafeAreaView, Image} from 'react-native';
 
 const App = () => {
 
-  //UseState
-  const [originalText, setText]= useState('Hello, World!')
-  //UseEffect
-  const [text, setText2]=useState(0);
-  useEffect(()=>{
-    console.log('The text has changed!')
-  },[text]);
-  useEffect(()=>{
-    console.log('The component has rendered!')
-  },[])
+  const [imageSource, setImageSource] = useState({uri:"http://example.com"});
+  return<SafeAreaView>
+      <Image 
+      source={imageSource} 
+      style={{width: 200,height:100, backgroundColor: 'red'}}
+      resizeMode={'stretch'}//stretch //repeat //center
+      onError={()=>{
+        console.log("Error has been detected while loading an image")
+        setImageSource(require("./assets/images/cake.png"))
+      }}
+    />
+      <Image 
+      source={{uri:"https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80"}} 
+      style={{width: 100,height:100}}
+    />
+  </SafeAreaView>
 
-  //useRef
-  let array = Array(10).fill(0);
-  const scrollViewRef= useRef(null);
-
-  const handleClick = () =>{
-    scrollViewRef.current.scrollTo({x:0,Y:0,animated:true});
-  }
-
-  //useContext
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
-    const toggleTheme = () =>{
-      setIsDarkMode(!isDarkMode);
-    }
-  //custom Hooks
-  const[isOn, toggleIsOn] = useToggle(false);
- 
-  return (
-    <SafeAreaView>
-      <View style={{backgroundColor:'blue', height:50}}>
-        <Text>This is going to be our heade container</Text>
-      </View>
-      <View style={{backgroundColor: 'grey'}}>
-        <MyText/>
-        <MyText/>
-        <MyText/>
-      </View>
-      <View>
-        <Text>All rights reserved</Text>
-      </View>
-      <Item name={'table'} price={20}></Item>
-      <Item name={'Chair'} price={100}></Item>
-      <Item name={'Desk'} price={200}></Item>
-      <View>
-        <MyText></MyText>
-      </View>
-      <View>
-        <Text
-          onPress={()=>setText('Hello World, I learned how to change state!')}>
-          {originalText}
-        </Text>
-      </View>
-      <View>
-        <Text
-          onPress={()=>setText2(text+1)}>
-          {text}
-        </Text>
-      </View>
-      <View>
-        <ScrollView ref={scrollViewRef}>
-          {array.map((value, index)=>(
-            <Text key={index}>Hello world! useRef{index}!</Text>
-          )
-          )}
-        </ScrollView>
-        <Button onPress={handleClick} title={'Scroll To Top'}/>
-      </View>
-      <ThemeContext.Provider value={isDarkMode}>
-        <View style={{backgroundColor: isDarkMode?'#222222':'#ffffff'}}>
-              <Text>Hello world!</Text>
-              <Button title={'Switch Mode'} onPress={toggleTheme}></Button>
-        </View>
-        <HomeScreen/>
-      </ThemeContext.Provider>
-      <View>
-          <Text>{isOn? 'ON': 'OFF'}</Text>
-          <Button title={'Toggle'} onPress={toggleIsOn}/>
-      </View>
-    </SafeAreaView>
-    
-  )
-
-}
+};
 
 export default App;
